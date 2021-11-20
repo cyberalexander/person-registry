4b706627989b0776d61424ca0c955b215f1c6d59
CyberAlexander <taurum13@gmail.com>
2021-11-20 09:52:08 +0300

    Rework switch statement. Part 2.

--- src/main/java/by/academy/it/loader/MenuLoader.java
+++ src/main/java/by/academy/it/loader/MenuLoader.java
@@ -1,18 +1,7 @@
 package by.academy.it.loader;
 
-import by.academy.it.factory.DaoFactory;
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
-
 import java.util.Scanner;
 
-import static by.academy.it.loader.AddressMenu.findAddress;
-import static by.academy.it.loader.DepartmentMenu.deleteDepartment;
-import static by.academy.it.loader.DepartmentMenu.findDepartment;
-import static by.academy.it.loader.DepartmentMenu.getDepartments;
-import static by.academy.it.loader.DepartmentMenu.loadDepartment;
-import static by.academy.it.loader.PersonMenu.flushPersonSession;
-import static by.academy.it.loader.PersonMenu.getAllPersons;
 import static java.lang.System.out;
 
 /**
@@ -20,81 +9,24 @@ import static java.lang.System.out;
  * Class for load menu in console and for execute operations with entities
  */
 public class MenuLoader {
-    private static final Logger log = LogManager.getLogger(MenuLoader.class);
-    private static Boolean showMenu = true;
-    protected final DaoFactory factory;
     private final Operator operator;
 
     public MenuLoader() {
-        this.factory = DaoFactory.getInstance();
         this.operator = new Operator();
     }
 
+    @SuppressWarnings("all")
     public void menu() {
         try (Scanner scanner = new Scanner(System.in)) {
-            while (showMenu) {
+            while (true) {
                 printMenu();
-                int operation = scanner.nextInt();
-
-                operator.operation(operation).ifPresent(op -> op.execute(scanner));
-
-                switch (operation) {
-                    case 2: // Get Person
-                        PersonMenu.findPerson(scanner);
-                        break;
-                    case 3: // Load Person
-                        PersonMenu.loadPerson(scanner);
-                        break;
-                    case 4: // Get all Persons
-                        getAllPersons();
-                        break;
-                    case 5: // Update Person
-                        PersonMenu.updatePerson(scanner);
-                        break;
-                    case 6: // Update Person Address
-                        PersonMenu.updatePersonAddress(scanner);
-                        break;
-                    case 7: // Delete Person
-                        PersonMenu.deletePerson(scanner);
-                        break;
-                    case 8: // Delete address
-                        AddressMenu.deleteAddress(scanner);
-                        break;
-                    case 9: // Get Address
-                        findAddress(scanner);
-                        break;
-                    case 10: // Create Department
-                        DepartmentMenu.createDepartment(scanner);
-                        break;
-                    case 11: // Get Department
-                        findDepartment(scanner);
-                        break;
-                    case 12: // Load Department
-                        loadDepartment(scanner);
-                        break;
-                    case 13: // Get all Departments
-                        getDepartments();
-                        break;
-                    case 14: // Update Department
-                        DepartmentMenu.updateDepartment(scanner);
-                        break;
-                    case 15: // Delete Department
-                        deleteDepartment(scanner);
-                        break;
-                    case 16:
-                        flushPersonSession();
-                        break;
-                    default:
-                        out.println("Choose proper number from the menu.");
-                        break;
-                }
-                showMenu = true;
+                operator.operation(scanner.nextInt()).ifPresent(op -> op.execute(scanner));
+                //TODO .orElse(() -> out.println("Choose proper number from the menu."));
             }
         }
     }
 
-
-    private static void printMenu() {
+    private void printMenu() {
         out.println("\n+--------------------------------------------------------------------------------------------+");
         out.println("|     Hello, " + System.getProperty("user.name") + "! You are in the application menu. Please, make your choice:  |");
         out.println("+---------------------------------------------------------------------------------------------------------------------------------------------------+");
--- src/main/java/by/academy/it/loader/Operator.java
+++ src/main/java/by/academy/it/loader/Operator.java
@@ -31,7 +31,22 @@ public class Operator {
 
     public enum Operation {
         EXIT(0, scanner -> System.exit(0)),
-        CREATE_PERSON(1, PersonMenu::createPerson);
+        CREATE_PERSON(1, PersonMenu::createPerson),
+        GET_PERSON(2, PersonMenu::findPerson),
+        LOAD_PERSON(3, PersonMenu::loadPerson),
+        GET_ALL_PERSONS(4, scanner -> PersonMenu.getAllPersons()),
+        UPDATE_PERSON(5, PersonMenu::updatePerson),
+        UPDATE_ADDRESS(6, PersonMenu::updatePersonAddress),
+        DELETE_PERSON(7, PersonMenu::deletePerson),
+        DELETE_ADDRESS(8, AddressMenu::deleteAddress),
+        FIND_ADDRESS(9, AddressMenu::findAddress),
+        CREATE_DEPARTMENT(10, DepartmentMenu::createDepartment),
+        FIND_DEPARTMENT(11, DepartmentMenu::findDepartment),
+        LOAD_DEPARTMENT(12, DepartmentMenu::loadDepartment),
+        FIND_DEPARTMENTS(13, scanner -> DepartmentMenu.getDepartments()),
+        UPDATE_DEPARTMENT(14, DepartmentMenu::updateDepartment),
+        DELETE_DEPARTMENT(15, DepartmentMenu::deleteDepartment),
+        FLUSH_SESSION_DEMO(16, scanner -> PersonMenu.flushPersonSession());
 
         private final Integer operationId;
         private final Consumer<Scanner> operator;
